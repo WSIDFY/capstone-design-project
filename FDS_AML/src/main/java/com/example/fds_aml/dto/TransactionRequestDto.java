4805612adf189160cf124e600836820dc153cfb7
yeeunnnnn <137869912+yeeunnnnn@users.noreply.github.com>
2026-04-11 01:18:23 +0900

    0411

--- FDS_AML/src/main/java/com/example/fds_aml/dto/TransactionRequestDto.java
+++ FDS_AML/src/main/java/com/example/fds_aml/dto/TransactionRequestDto.java
@@ -18,7 +18,6 @@ public class TransactionRequestDto {
     private Double oldbalanceDest;
     private Double newbalanceDest;
 
-    // 파이썬에서 쏘는 시간 형식을 스프링이 찰떡같이 알아먹도록 설정
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime transactionDate;
 }
