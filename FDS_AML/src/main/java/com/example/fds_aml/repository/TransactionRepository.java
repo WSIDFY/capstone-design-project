3eddb36f8fc6f7ef3adc67e98c377adb5cb487a8
K_blueprint <141166836+WSIDFY@users.noreply.github.com>
2026-05-05 20:59:20 +0900

    Delete FDS_AML directory

--- FDS_AML/src/main/java/com/example/fds_aml/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fds_aml.repository;
-
-import com.example.fds_aml.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
